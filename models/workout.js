6d3e23ce6d9172cebda5449c76011eea185b17bd
Philip Ng <phil__n@hotmail.com>
2020-04-12 22:35:49 -0400

    api for get and post requests made

--- models/workout.js
+++ models/workout.js
@@ -2,29 +2,33 @@ const mongoose = require("mongoose"),
   Schema = mongoose.Schema;
 
 const workoutSchema = new Schema({
-  day: { type: Date, default: () => new Date() },
+  day: { type: Date, default: Date.now },
   exercises: [
     {
       type: { type: String, trim: true, required: "Enter an exercise type" },
       name: { type: String, trim: true, required: "Enter an exercise name" },
       distance: {
         type: Number,
+        minimum: 1,
       },
       duration: {
         type: Number,
+        minimum: 1,
         required: "Enter the duration of your exercise in minutes",
       },
       weight: {
         type: Number,
+        minimum: 1,
       },
       sets: {
         type: Number,
+        minimum: 1,
       },
-      reps: { type: Number },
+      reps: { type: Number, minimum: 1 },
     },
   ],
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
 
-module.exports = { Workout: Workout };
+module.exports = Workout;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,2 +1,19 @@
 const router = require("express").Router();
-const 
+const Workout = require("../models/workout");
+
+router.get("/api/workouts", (req, res) => {
+  Workout.find()
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.post("/api/workouts", (req, res) => {
+  Workout.create({})
+    .then((result) => res.json(result))
+    .catch((err) => res.json(err));
+});
+
