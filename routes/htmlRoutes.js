2e042f19c278f34f8a37a7b3ec822b81f47ff7a7
Philip Ng <phil__n@hotmail.com>
2020-04-13 00:34:25 -0400

    html routes were serving the wrong pages; fixed now

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
 const path = require("path");
 
-router.get("/", (req, res) => {
+router.get("/exercise", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
 
-router.get("/exercise", (req, res) => {
+router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
