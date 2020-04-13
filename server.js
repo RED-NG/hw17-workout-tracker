41d233112a94e172f1db6b7e28919d97b9daf13b
Philip Ng <phil__n@hotmail.com>
2020-04-13 01:06:36 -0400

    added logs for whether database connected successfully or not

--- server.js
+++ server.js
@@ -12,14 +12,20 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI ||
-    "mongodb://<user1>:<password1>@ds229108.mlab.com:29108/heroku_sn6wbnxt",
-  {
-    useNewUrlParser: true,
-    useFindAndModify: false,
-  }
-);
+mongoose
+  .connect(
+    process.env.MONGODB_URI ||
+      "mongodb://<user1>:<password1>@ds229108.mlab.com:29108/heroku_sn6wbnxt",
+    {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useFindAndModify: false,
+    }
+  )
+  .then(() => console.log("DB connected successfully!"))
+  .catch((err) => {
+    console.log("DB connection error: " + err);
+  });
 
 app.use(require("./routes/apiRoutes"));
 app.use(require("./routes/htmlRoutes"));
