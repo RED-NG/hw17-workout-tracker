fef13922da49a69b032ed98f02947a3e5953b035
Philip Ng <phil__n@hotmail.com>
2020-04-13 00:33:35 -0400

    finished api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,10 @@
 const router = require("express").Router();
-const Workout = require("../models/workout");
+const Workout = require("../models/workout.js");
 
 router.get("/api/workouts", (req, res) => {
   Workout.find()
-    .then((result) => {
-      res.json(result);
+    .then((data) => {
+      res.json(data);
     })
     .catch((err) => {
       res.status(400).json(err);
@@ -13,7 +13,24 @@ router.get("/api/workouts", (req, res) => {
 
 router.post("/api/workouts", (req, res) => {
   Workout.create({})
-    .then((result) => res.json(result))
+    .then((data) => res.json(data))
     .catch((err) => res.json(err));
 });
 
+router.put("/api/workouts/:id", (req, res) => {
+  Workout.findByIdAndUpdate(req.params.id, { $push: { exercises: req.body } })
+    .then((data) => res.json(data))
+    .catch((err) => res.status(400).json(err));
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({}, (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.json(data);
+    }
+  });
+});
+
+module.exports = router;
